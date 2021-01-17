0fee8a08a2526d69e0ebeccf97bf181e9f0dab9f
alessio rea <alexei.80@hotmail.fr>
2021-01-17 14:32:22 +0100

    deleted function model_prediction

--- chaos/application/server.py
+++ chaos/application/server.py
@@ -26,30 +26,6 @@ def __remove_accents__(df) :
         return new_df
 
 
-def model_prediction(data_file_path, model_file_path, save_file) :
-    """
-    Produces a csv files from raw_data thanks to provided model
-    csv file is to be found in output directory and has two new columns
-    One predictions column and one probability column
-    """
-    with open(model_file_path, 'rb') as pickle_file:
-        model = pickle.load(pickle_file)
-
-    def probability_predictions(model, data, raw_data) :
-        new_X = raw_data.copy()
-        new_X[cf.PRED_PROBA_COL_NAME] = model.predict_proba(data)[:,1]
-        new_X[cf.PRED_COL_NAME] = model.predict(data)
-        return new_X
-
-    #print(data_file_path)
-
-    #cd = CleanDataTransformer(path=data_file_path)
-    #data = cd.load_cleaned_data()
-    #raw_data = cd.load_raw_data()l
-
-    #preds = probability_predictions(model, data, raw_data)
-    #preds.to_csv(os.path.join(cf.OUTPUTS_DIR, save_file))
-
 
 @app.route("/pred", methods=["POST"])
 def pred():
