1154d7af406d9a48e651b6c8393f27dfc66e427f
alessio rea <alexei.80@hotmail.fr>
2021-01-18 20:15:37 +0100

    moved code from unit to functional

--- chaos/test/functional/test_server.py
+++ chaos/test/functional/test_server.py
@@ -42,3 +42,32 @@ def test_example3():
     results = response.json()
     
     assert results["answer"] == 0
+
+
+def test_preds_are_probabilities():
+    endpoint = "preds"
+    host = "0.0.0.0"
+    port = 5000
+    url = f"http://{host}:{port}/{endpoint}"
+    
+    headers = {'Content-Type': 'application/json' } 
+    resp = requests.post(url, headers=headers, data=json.dumps(multiple_lead,indent=4))
+    assert resp.status_code == 200
+    resp_body = resp.json()
+    assert resp_body["1"]['predict_proba'] >= 0
+    assert resp_body["2"]['predict_proba'] <= 1
+
+
+
+def test_pred_is_probability():
+    endpoint = "pred"
+    host = "0.0.0.0"
+    port = 5000
+    url = f"http://{host}:{port}/{endpoint}"
+    
+    headers = {'Content-Type': 'application/json' } 
+    resp = requests.post(url, headers=headers, data=json.dumps(unique_lead,indent=4))
+    assert resp.status_code == 200
+    resp_body = resp.json()
+    assert resp_body['prediction'] >= 0
+    assert resp_body['prediction'] <= 1
--- chaos/test/unit/test_hello_world.py
+++ chaos/test/unit/test_hello_world.py
@@ -9,26 +9,14 @@ from pathlib import Path
 from flask import Flask, jsonify, request
 
 
-def test_preds_are_probabilities():
-    url = 'http://0.0.0.0:5000/preds'
-    
-    headers = {'Content-Type': 'application/json' } 
-    resp = requests.post(url, headers=headers, data=json.dumps(multiple_lead,indent=4))
-    assert resp.status_code == 200
-    resp_body = resp.json()
-    assert resp_body["1"]['predict_proba'] >= 0
-    assert resp_body["2"]['predict_proba'] <= 1
+
+def test_model_file_exists():
+    model = os.path.join(chaos.__path__[0], "domain/model.pkl")
+    assert os.path.isfile(model)
 
 
 
-def test_pred_is_probability():
-    url = 'http://0.0.0.0:5000/pred'
-    
-    headers = {'Content-Type': 'application/json' } 
-    resp = requests.post(url, headers=headers, data=json.dumps(unique_lead,indent=4))
-    assert resp.status_code == 200
-    resp_body = resp.json()
-    assert resp_body['prediction'] >= 0
-    assert resp_body['prediction'] <= 1
+
+
 
 
